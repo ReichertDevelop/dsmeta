ee113f927121f0f2ad717f0bf4f25c09ba8786eb
ReichertDevelop <reichert.develop@gmail.com>
2022-07-17 21:25:47 -0300

    Date update

--- frontend/src/components/salesCard/index.tsx
+++ frontend/src/components/salesCard/index.tsx
@@ -18,7 +18,10 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-      axios.get(`${BASE_URL} /sales`)
+
+      const dmin = minDate.toISOString().slice(0, 10);
+      const dmax = minDate.toISOString().slice(0, 10);
+      axios.get(`${BASE_URL} /sales?minDate=${dmin}-01&maxDate=${dmax}`)
         .then(response => {
           setSales(response.data.content);
         })
